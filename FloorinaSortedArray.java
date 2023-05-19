db0ffa8965b49b198b5d0255b8e4cd1c6d89b5ac
Tejas Kulkarni <133078477+TejuGubbu@users.noreply.github.com>
2023-05-19 16:43:15 +0530

    commit

---
+++ FloorinaSortedArray.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.io.*;
+
+public class FloorinaSortedArray
+{
+    public static void main(String args[]) 
+	{
+        //your code here
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int key = sc.nextInt();
+		int a[] = new int[n];
+		for(int i = 0 ;  i<n ; i++)
+			{
+				a[i] = sc.nextInt();
+			}
+		System.out.print(find(a,key));
+    }
+	public static int find(int a[] , int key)
+	{
+		int si = 0;
+		int ei = a.length-1;
+		while(si<=ei)
+			{
+				int mid = (si+ei)/2;
+				if(a[mid]==key)
+				{
+					return mid;
+				}
+				else if(a[mid]>key)
+				{
+					ei = mid-1;
+				}
+				else
+				{
+					si = mid+1;
+				}
+			}
+		if(ei<si)
+		{
+			return ei;
+		}
+		return -1;
+	}
+}
